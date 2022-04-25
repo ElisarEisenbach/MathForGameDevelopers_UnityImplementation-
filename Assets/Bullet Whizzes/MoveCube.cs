9eee4e63a1ea408ff9dc4052c1899b63aa4c1b25
elisarei <elisarei@matrix.co.il>
2022-04-25 17:37:43 +0300

    Start working on Bullet Whizzes

---
+++ Assets/Bullet Whizzes/MoveCube.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    public SpriteRenderer Line;
+    public Vector3 Velocity;
+
+    private Interoplate interoplate;
+    private float top;
+    private float buttom;
+    bool isUp = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interoplate = GetComponent<Interoplate>();
+        top = Line.bounds.max.y;
+        buttom = Line.bounds.min.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 0;
+        if (isUp)
+        {
+            x = interoplate.Approach(top, transform.position.y, .02f);
+            if (x == top)
+            {
+                isUp = false;
+            }
+        }
+        else
+        {
+            x = interoplate.Approach(buttom, transform.position.y, .02f);
+            if (x == buttom)
+            {
+                isUp = true;
+            }
+        }
+
+        transform.position = new Vector3(transform.position.x, x, transform.position.z);
+
+    }
+}
---
+++ Assets/Bullet Whizzes/Projection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projection : MonoBehaviour
+{
+
+    public SpriteRenderer GameObjectToFind;
+    public SpriteRenderer Line;
+
+    private Vector3 bulletStartPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletStartPos = GameObjectToFind.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 b = transform.position - bulletStartPos ;
+        }
+    }
+}
