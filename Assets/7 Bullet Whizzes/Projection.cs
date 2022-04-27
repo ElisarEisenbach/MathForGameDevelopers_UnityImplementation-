c25f7016ddb46079e4024a71de4f0ece84fe7e5a
elisarei <elisarei@matrix.co.il>
2022-04-27 17:26:36 +0300

    Working projections on update

--- Assets/7 Bullet Whizzes/Projection.cs
+++ Assets/7 Bullet Whizzes/Projection.cs
@@ -9,16 +9,62 @@ public class Projection : MonoBehaviour
 
     private Vector3 a,e,b,bPrime;
     private float x;
+    private int clicks = 0;
 
     // Start is called before the first frame update
     void Start()
+    {
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (clicks == 0)
+            {
+                firstPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                firstPoint.z = 0;
+                CreatePrimitiveInClickedPos(firstPoint);
+                clicks++;
+            }
+            else if (clicks == 1)
+            {
+                secondPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                secondPoint.z = 0;
+                CreatePrimitiveInClickedPos(secondPoint);
+                clicks = 0;
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            e = GetE();
+
+            CreateLine(firstPoint, secondPoint);
+
+            CreateLine(transform.position, firstPoint + bPrime);
+
+        }
+    }
+
+    private Vector3 GetE()
     {
         a = secondPoint - firstPoint;
         b = transform.position - firstPoint;
         x = Vector3.Dot(a, b) / Vector3.Dot(a, a);
         bPrime = x * a;
-        e = bPrime - b;
-        Debug.Log("Closest point is " + firstPoint + e);
+        return (bPrime - b);
+    }
+
+    private void CreateLine(Vector3 start, Vector3 end)
+    {
         LineRenderer lineRenderer = null;
         lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
         lineRenderer.startColor = Color.black;
@@ -29,37 +75,16 @@ public class Projection : MonoBehaviour
         lineRenderer.useWorldSpace = true;
 
         //For drawing line in the world space, provide the x,y,z values
-        lineRenderer.SetPosition(0,firstPoint); //x,y and z position of the starting point of the line
-        lineRenderer.SetPosition(1, secondPoint); //x,y and z position of the end point of the line
+        lineRenderer.SetPosition(0, start); //x,y and z position of the starting point of the line
+        lineRenderer.SetPosition(1, end); //x,y and z position of the end point of the line
         lineRenderer.startWidth = .2f;
         lineRenderer.endWidth = .2f;
-
-
-        LineRenderer ShootLine = null;
-        ShootLine = new GameObject("Line").AddComponent<LineRenderer>();
-        ShootLine.startColor = Color.black;
-        ShootLine.endColor = Color.black;
-        ShootLine.startWidth = 0.01f;
-        ShootLine.endWidth = 0.01f;
-        ShootLine.positionCount = 2;
-        ShootLine.useWorldSpace = true;
-
-        //For drawing line in the world space, provide the x,y,z values
-        ShootLine.SetPosition(0, transform.position); //x,y and z position of the starting point of the line
-        ShootLine.SetPosition(1, firstPoint + bPrime); //x,y and z position of the end point of the line
-        ShootLine.startWidth = .2f;
-        ShootLine.endWidth = .2f;
-        ShootLine.startColor = Color.black;
-        ShootLine.endColor = Color.black;
-
-
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreatePrimitiveInClickedPos(Vector3 pos)
     {
-        while (Input.GetKeyDown(KeyCode.Space))
-        {
-        }
+        var primitive = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        primitive.transform.position = pos;
+        primitive.transform.localScale = new Vector3(.2f, .2f, .2f);
     }
 }
