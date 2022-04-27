285abf7fde1d6141edd72e7147e0e048fca6146e
elisarei <elisarei@matrix.co.il>
2022-04-27 20:00:52 +0300

    Working billboarding add meta files to gitifnore

---
+++ Assets/8 Billboarding/CreateMesh.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class CreateMesh : MonoBehaviour
+{
+
+    Mesh mesh;
+
+   public Vector3[] vertices;
+    int[] triangles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateShape();
+        UpdateMesh();
+    }
+
+    public void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3 (0,0,0),
+            new Vector3 (0,1,0),
+            new Vector3 (1,0,0),
+            new Vector3 (1,1,0)
+
+        };
+
+        triangles = new int[]
+        {
+            0,1,2,
+            1,3,2
+        };
+
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            vertices[2] = new Vector3(1, 2, 2);
+            UpdateMesh();
+        }
+    }
+
+    // Update is called once per frame
+
+}
---
+++ Assets/8 Billboarding/billboarding.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billboarding : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    MeshFilter mesh;
+    CreateMesh createMesh;
+
+    Vector3 f, r, u;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>();
+        createMesh = GetComponent<CreateMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        f = (transform.position - player.transform.position);
+        r = Vector3.Cross(-Vector3.up, f).normalized;
+        u = Vector3.Cross(f, -r).normalized;
+
+       createMesh.vertices[0] = (-u - r);
+       createMesh.vertices[1] = (r - u);
+       createMesh.vertices[2] = (u - r);
+        createMesh.vertices[3] = (u + r);
+        createMesh.UpdateMesh();
+        
+
+    }
+}
+
